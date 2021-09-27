39971dec31d15ff3d0ed0ad57001cddb42a4c2f7
Gydizu <91119269+Gydizu@users.noreply.github.com>
2021-09-27 18:16:32 +0700

    Add files via upload

---
+++ Факультатив ООП д.з. 1/Finding_max_by_GR.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Finding_the_Extremum
+{
+    class FindingMaxByGR
+    {
+        private const double PHI = 1.6180339887498949;
+        private const double E = 0.001;
+        static public double Function(double x/*, string function*/) => Math.Pow(x + 1, 3) + (5 * Math.Pow(x, 2));
+        static public double CalculatorMax(double a, double b/*, string function*/)
+        {
+
+            while (!(Math.Abs(b - a) < E))
+                if (Function((b - ((b - a) / PHI))/*, function*/)<=Function((a + ((b - a) / PHI))/*,function*/))
+                    a = b - ((b - a) / PHI);
+                else
+                    b = a + ((b - a) / PHI);
+            return (Function((a + b) / 2));
+        }
+        static public double CalculatorMin(double a, double b/*, string function*/)
+        {
+            while (!(Math.Abs(b - a) < E))
+                if (Function((b - ((b - a) / PHI))) >= Function((a + ((b - a) / PHI))))
+                    a = b - ((b - a) / PHI);
+                else
+                    b = a + ((b - a) / PHI);
+            return (Function((a + b) / 2));
+        }
+    }
+}
---
+++ Факультатив ООП д.з. 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Finding_the_Extremum
+{
+    class MainProgram
+    {
+        static void Main()
+        {
+            float a, b;
+            //string function;
+            //Console.Write("Введите исследуемую фукцию: ");
+            //function = Console.ReadLine();
+            //Console.Write("Введите исследуемые границы: ");
+            //a = Console.Read();
+            //b = Console.Read();
+            a = -2;
+            b = 2;
+            Console.WriteLine($"Экстремумы (методом Золотого сечения) для функции f(x)=(x+1)^3+5*x^2:\n{FindingMaxByGR.CalculatorMax(a, b/*, function*/):0.000} - максимум функции на отрезке [{a};{b}]\n{FindingMaxByGR.CalculatorMin(a, b/*, function*/):0.000} - минимум функции на отрезке [{a};{b}]");
+            Console.WriteLine();
+            Console.WriteLine($"Экстремумы (дихотомическим методом) для функции f(x)=(x+1)^3+5*x^2:\n{Find_ext_by_dih.Calculate_Max(a, b/*, function*/):0.000} - максимум функции на отрезке [{a};{b}]\n{Find_ext_by_dih.Calculate_Min(a, b/*, function*/):0.000} - минимум функции на отрезке [{a};{b}]");
+            Console.WriteLine();
+            Console.WriteLine();
+            a = -8;
+            b = 0;
+            Console.WriteLine($"Экстремумы (методом Золотого сечения) для функции f(x)=(x+1)^3+5*x^2:\n{FindingMaxByGR.CalculatorMax(a, b/*, function*/):0.000} - максимум функции на отрезке [{a};{b}]\n{FindingMaxByGR.CalculatorMin(a, b/*, function*/):0.000} - минимум функции на отрезке [{a};{b}]");
+            Console.WriteLine();
+            Console.WriteLine($"Экстремумы (дихотомическим методом) для функции f(x)=(x+1)^3+5*x^2:\n{Find_ext_by_dih.Calculate_Max(a, b/*, function*/):0.000} - максимум функции на отрезке [{a};{b}]\n{Find_ext_by_dih.Calculate_Min(a, b/*, function*/):0.000} - минимум функции на отрезке [{a};{b}]");
+        }
+    }
+}
